82dbb0baca93d3198a707326360f5f0d30ff7d9b
skyino7 <christambassador12@gmail.com>
2023-06-15 05:18:58 -0700

    prints exactly with proper grammar, but the outcome is a piece of art,

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Function
+ *
+ * Prints exactly - with proper grammar, but the outcome is a piece of art,
+ *
+ * return: 0 when successful
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
