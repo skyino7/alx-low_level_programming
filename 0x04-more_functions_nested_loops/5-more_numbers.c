c38314da3940d7fecc5353757fdcd70a37565351
skyino7 <christambassador12@gmail.com>
2023-06-22 16:08:38 -0700

    Write a function that prints the numbers, from 0 to 9, Do not print 2 and 4.

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,14 +1,19 @@
 #include "main.h"
 
+/**
+ * more_numbers - prototype function
+ *
+ */
+
 void more_numbers(void)
 {
-	int i,j;
+	int i, j;
 
 	for (i = 0; i < 10; i++)
 	{
 		for (j = 0; j <= 14; j++)
 		{
-			if (j >9)
+			if (j > 9)
 			{
 				_putchar('0' + (j / 10));
 			}
