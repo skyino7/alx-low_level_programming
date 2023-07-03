456f9a93adc57cbe0d7aed776eb9a941a78dd757
Skyino7 <christambassador12@gmail.com>
2023-07-03 11:48:30 +0000

    The _memset() function fills the first n bytes of the memory area pointed to by s with the constant byte b and Returns a pointer to the memory area s.

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _memset - function
+ * @s: pointer
+ * @b: character
+ * @n: integer
+ *
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+
+	unsigned int i;
+
+	for (i = 0; i < n; i++) {
+		s[i] = b;
+	}
+
+	return (s);
+	
+}
