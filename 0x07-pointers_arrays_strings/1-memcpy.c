2e8dedbee527514fbb644ff8b66867e887cdfc9d
Skyino7 <christambassador12@gmail.com>
2023-07-03 12:00:22 +0000

    The _memcpy() function copies n bytes from memory area src to memory area dest and Returns a pointer to dest.

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _memcpy - function
+ *
+ * @dest: pointer
+ * @src: pointer
+ * @n: integer
+ *
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	char *cdest = dest;
+	char *csrc = src;
+
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		cdest[i] = csrc[i];
+	}
+
+	return (dest);
+}
