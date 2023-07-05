4131e15682629327de3a07a3902f6c59846c1dd7
Skyino7 <christambassador12@gmail.com>
2023-07-05 17:09:46 +0000

    Write a function that returns the length of a string.

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -5,16 +5,17 @@
  *
  * @s: pointer
  *
+ * Return: 0 when successful
  */
 
 int _strlen_recursion(char *s)
 {
 	if (*s == '\0')
 	{
-		return 0;
+		return (0);
 	}
 	else
 	{
-		return 1 + _strlen_recursion(s + 1);
+		return (1 + _strlen_recursion(s + 1));
 	}
 }
