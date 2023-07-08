d9cd34c864ef8182636aa82a40b999e6e90e4c68
Skyino7 <christambassador12@gmail.com>
2023-07-08 00:08:15 +0000

    Argument Count and Argument Vector Task

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - function
+ * @argc: number of arguments passed
+ * @argv: arguments vector
+ *
+ * Return: 0 for success
+ */
+
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
