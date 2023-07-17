906037932f70b47bc62e0b7fe323f43a1271577c
Skyino7 <christambassador12@gmail.com>
2023-07-17 11:09:20 +0000

    Preprocessor to include files directly into other files, or #define constants.

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - function
+ *
+ * Return: 0 when successful
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+
+	return (0);
+}
---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,4 @@
+#ifndef _ABS_MACRO_H
+	#define _ABS_MACRO_H
+	#define ABS(x) ((x < 0)?(-x):(x))
+#endif
