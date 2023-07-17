72d9f2b53207cfc656a2101480d1291f0cdc0cdd
Skyino7 <christambassador12@gmail.com>
2023-07-17 22:08:55 +0000

    freeing the dog

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,20 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * free_dog - free dog function
+ * @d: pointer
+ */
+
+void free_dog(dog_t *d)
+{
+	if (d == NULL)
+	{
+		return;
+	}
+
+	free(d->name);
+	free(d->owner);
+	free(d);
+
+}
