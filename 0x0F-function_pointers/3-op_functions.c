02bb2f46c21cdc8b4854f1648405701828978ba7
Skyino7 <christambassador12@gmail.com>
2023-07-19 23:01:32 +0000

    Revert "Write a program that performs simple operations."
    This reverts commit 1fce0d9de33deed9e3bdadd505998248378409a9.

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,11 +1,12 @@
 #include "3-calc.h"
 
 /**
- * op_add - addition
- * @a: the first number
- * @b: the second number
- * Return: Addition of a and b
+ * op-add - add operation
+ * @a: The first number
+ * @b: The second number
+ * Return: The sum of a + b
  */
+
 int op_add(int a, int b)
 {
 	return (a + b);
@@ -17,6 +18,7 @@ int op_add(int a, int b)
  * @b: the second number
  * Return: The difference of a and b
  */
+
 int op_sub(int a, int b)
 {
 	return (a - b);
@@ -28,6 +30,7 @@ int op_sub(int a, int b)
  * @b: The second number
  * Return: the product of a and b
  */
+
 int op_mul(int a, int b)
 {
 	return (a * b);
@@ -39,6 +42,7 @@ int op_mul(int a, int b)
  * @b: The second number
  * Return: The division of a to b
  */
+
 int op_div(int a, int b)
 {
 	return (a / b);
@@ -50,6 +54,7 @@ int op_div(int a, int b)
  * @b: The Second number
  * Return: The remainder
  */
+
 int op_mod(int a, int b)
 {
 	return (a % b);
