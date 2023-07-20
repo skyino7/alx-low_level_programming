57d1c80937d7f33b38cd4a5d316c5e39c74d5230
Skyino7 <christambassador12@gmail.com>
2023-07-20 15:43:35 +0000

    Write a function that prints strings.

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -37,7 +37,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 
 		if (i < n - 1)
 		{
-			if (separator)
+			if (separator != NULL)
 			{
 				printf("%s", separator);
 			}
