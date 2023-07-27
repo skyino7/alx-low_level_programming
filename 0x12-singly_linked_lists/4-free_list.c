c702a11046bbc4397b4e1d96479866261b1758eb
Skyino7 <christambassador12@gmail.com>
2023-07-27 20:32:33 +0000

    Frees a list_t list.

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * free_list - function
+ * @head: head node
+*/
+
+void free_list(list_t *head)
+{
+	list_t *current;
+
+	while (head != NULL)
+	{
+		current = head;
+		head = head->next;
+		free(current->str);
+		free(current);
+	}
+
+}
