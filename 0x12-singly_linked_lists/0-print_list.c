4edba05327281a629b0fcf89223b39bf68af7b01
Skyino7 <christambassador12@gmail.com>
2023-07-27 18:40:03 +0000

    Prints all the elements of a list_t list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -8,22 +8,21 @@
 
 size_t print_list(const list_t *h)
 {
-    size_t cnt = 0;
 
-    while (h != NULL)
-    {
+	size_t cnt = 0;
 
-        if (h->str == NULL)
-        {
-            printf("[0] (nil)\n");
-        }
-        else
-            printf("[%d] %s\n", h->len, h->str);
-        
-        cnt++;
-        h = h->next;
-        
-    }
-
-    return (cnt);
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+		{
+			printf("[0] (nil)\n");
+		}
+		else
+		{
+			printf("[%d] %s\n", h->len, h->str);
+		}
+		cnt++;
+		h = h->next;
+	}
+	return (cnt);
 }
