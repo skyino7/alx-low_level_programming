f5f53c0398f550814b58bca74938a30bbe8cd2cb
Skyino7 <christambassador12@gmail.com>
2023-08-01 22:29:13 +0000

    requirement fail fixed

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,20 +1,26 @@
 #include "lists.h"
 
+/**
+ * free_listint2 - function
+ * @head: head node
+*/
+
 void free_listint2(listint_t **head)
 {
-    listint_t *temp, *current;
+	listint_t *temp, *current;
 
-    if (head == NULL || *head == NULL)
-        return;
-    
-    current = *head;
+	if (head == NULL || *head == NULL)
+		return;
 
-    while (current != NULL)
-    {
-        temp = current;
-        current = current->next;
-        free(temp);
-    }
-    
-    *head = NULL;
+	current = *head;
+
+	while (current != NULL)
+	{
+		temp = current;
+		current = current->next;
+		free(temp);
+	}
+
+	*head = NULL;
+
 }
