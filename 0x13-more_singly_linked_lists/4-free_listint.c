57031b01a421ecabe3854f5809ca2f6c6975f7e2
Skyino7 <christambassador12@gmail.com>
2023-08-01 19:57:28 +0000

    requirement fail fixed

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -1,16 +1,21 @@
 #include "lists.h"
 
+/**
+ * free_listint - function
+ * @head: head node
+ */
+
 void free_listint(listint_t *head)
 {
-    listint_t *temp, *current;
-    
-    temp = head;
+	listint_t *temp, *current;
 
-    while (temp != NULL)
-    {
-        current = temp;
-        temp = temp->next;
-        free(current);
-    }
-    
+	temp = head;
+
+	while (temp != NULL)
+	{
+		current = temp;
+		temp = temp->next;
+		free(current);
+	}
+
 }
