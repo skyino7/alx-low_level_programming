7e568850476508a9ccc81c0947d1c580bdc58d83
Skyino7 <christambassador12@gmail.com>
2023-08-03 19:14:57 +0000

    requirement fail fixed

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -4,14 +4,16 @@
  * get_bit - function
  * @n: number
  * @index: index
+ * Return: n >> index
 */
 
 int get_bit(unsigned long int n, unsigned int index)
 {
-    if (index >= sizeof(unsigned long int) * 8)
-    {
-        return (-1);
-    }
-    return (n >> index) & 1;
-    
+	if (index >= sizeof(unsigned long int) * 8)
+	{
+		return (-1);
+	}
+
+	return ((n >> index) & 1);
+
 }
