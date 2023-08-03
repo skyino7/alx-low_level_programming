f3e0fb61f1da0bd70026c87b734b98bacd19ee86
Skyino7 <christambassador12@gmail.com>
2023-08-03 21:28:41 +0000

    sets the value of a bit to 1 at a given index.

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * set_bit - function
+ * @n: number
+ * @index: index
+ * Return: -1 or 1
+*/
+
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	if (index >= sizeof(unsigned long int) * 8)
+	{
+		return (-1);
+	}
+	*n |= (1UL << index);
+
+	return (1);
+
+}
