f5cfb1b35fb34e032200445b55820ddfb94b69ec
Skyino7 <christambassador12@gmail.com>
2023-09-14 19:16:15 +0000

    Betty Errors Corrected

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -1,33 +1,39 @@
 #include "lists.h"
 
+/**
+ * add_dnodeint_end - function
+ * @head: head node
+ * @n: number
+ * Return: New node
+*/
+
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
-    dlistint_t *new;
-    dlistint_t * curr;
+	dlistint_t *new;
+	dlistint_t *curr;
 
-    new = malloc(sizeof(dlistint_t));
+	new = malloc(sizeof(dlistint_t));
 
-    if (new == NULL)
-        return (NULL);
+	if (new == NULL)
+		return (NULL);
 
-    new->n = n;
-    new->prev = NULL;
-    new->next = NULL;
+	new->n = n;
+	new->prev = NULL;
+	new->next = NULL;
 
-    if (*head == NULL)
-        *head = new;
-    else
-    {
-        curr = *head;
+	if (*head == NULL)
+		*head = new;
+	else
+	{
+		curr = *head;
 
-        while (curr->next != NULL)
-            curr = curr->next;
+		while (curr->next != NULL)
+			curr = curr->next;
 
-        curr->next = new;
-        new->prev = curr;
+		curr->next = new;
+		new->prev = curr;
+	}
 
-    }
-
-    return (new);
+	return (new);
 
 }
