6185f7f6c91fa6ac4607f650e62358a5159f1df2
Skyino7 <christambassador12@gmail.com>
2023-10-05 19:10:36 +0000

    Write a function that adds an element to the hash table.

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -9,12 +9,27 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 unsigned long int index;
-hash_node_t *new = NULL;
+hash_node_t *new = NULL, *curr = NULL;
 
 if (ht == NULL || key == NULL || value == NULL || key[0] == '\0')
 return (0);
 
 index = key_index((const unsigned char *) key, ht->size);
+
+curr = ht->array[index];
+
+while (curr)
+{
+if (strcmp(curr->key, key) == 0)
+{
+free(curr->value);
+curr->value = strdup(value);
+if (curr->next == NULL)
+return(0);
+}
+curr = curr->next;
+}
+
 
 new = (hash_node_t *)malloc(sizeof(hash_node_t));
 
