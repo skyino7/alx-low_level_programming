1cc4a5805324164c8c88efe4666bc0912fb4a68b
Seth Ketsi <christambassador12@gmail.com>
2024-01-15 11:04:12 +0000

    betty errors corrected

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -10,36 +10,36 @@
 
 int binary_search(int *array, size_t size, int value)
 {
-    int low, high, mid, i;
+	int low, high, mid, i;
 
-    if (array == NULL)
-        return (-1);
+	if (array == NULL)
+		return (-1);
 
-    low = 0;
-    high = size - 1;
+	low = 0;
+	high = size - 1;
 
-    while (low <= high)
-    {
+	while (low <= high)
+	{
 
-        mid = (low + high) / 2;
+		mid = (low + high) / 2;
 
-        printf("Searching in array: ");
-        for (i = low; i <= high; i++) {
-            printf("%d", array[i]);
-            if (i < high) {
-                printf(", ");
-            }
-        }
-        printf("\n");
+		printf("Searching in array: ");
 
-        if (array[mid] == value)
-            return (mid);
-        else if (array[mid] < value)
-            low = mid + 1;
-        else
-            high = mid - 1;
+		for (i = low; i <= high; i++)
+		{
+			printf("%d", array[i]);
+			if (i < high)
+				printf(", ");
+		}
+		printf("\n");
 
-    }
+		if (array[mid] == value)
+			return (mid);
+		else if (array[mid] < value)
+			low = mid + 1;
+		else
+			high = mid - 1;
+	}
 
-    return (-1);
+	return (-1);
 }
