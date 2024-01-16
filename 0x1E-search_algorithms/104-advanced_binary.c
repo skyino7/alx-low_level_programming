a0e8960fb25c27f151d2b32a024a1c5a9ad3880b
Seth Ketsi <christambassador12@gmail.com>
2024-01-16 19:05:30 +0000

    updated

--- 0x1E-search_algorithms/104-advanced_binary.c
+++ 0x1E-search_algorithms/104-advanced_binary.c
@@ -13,31 +13,31 @@
 
 int advanced_binary(int *array, size_t size, int value)
 {
-	size_t low = 0, high = size - 1, mid, i;
+size_t low = 0, high = size - 1, mid, i;
 
-	if (array == NULL || size == 0)
-		return (-1);
+if (array == NULL || size == 0)
+return (-1);
 
-	while (low <= high)
-	{
-		printf("Searching in array: ");
-		for (i = low; i <= high; i++)
-		{
-			printf("%d", array[i]);
-			if (i < high)
-				printf(", ");
-			else
-				printf("\n");
-		}
-		mid = (low + high) / 2;
-        if (array[mid] < value)
-            low = mid + 1;
-        else if (array[mid] > value)
-            high = mid;
-        else if (mid == 0 || array[mid - 1] != value)
-            return (mid);
-        else
-            high = mid;
-	}
-	return (-1);
+while (low <= high)
+{
+printf("Searching in array: ");
+for (i = low; i <= high; i++)
+{
+printf("%d", array[i]);
+if (i < high)
+printf(", ");
+else
+printf("\n");
+}
+mid = (low + high) / 2;
+if (array[mid] < value)
+low = mid + 1;
+else if (array[mid] > value)
+high = mid;
+else if (mid == 0 || array[mid - 1] != value)
+return (mid);
+else
+high = mid;
+}
+return (-1);
 }
