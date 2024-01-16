cfc7236191ee4d12f9f20f6a8c0bfadc5425ba8d
Seth Ketsi <christambassador12@gmail.com>
2024-01-16 18:56:09 +0000

    searches for a value in a sorted array of integers using the Binary search algorithm

---
+++ 0x1E-search_algorithms/104-advanced_binary.c
@@ -0,0 +1,41 @@
+#include "search_algos.h"
+
+/**
+ * advanced_binary - searches for a value in a sorted array of integers
+ *                   using the Binary search algorithm
+ *
+ * @array: pointer to the first element of the array to search in
+ * @size: number of elements in the array
+ * @value: value to search for
+ *
+ * Return: first index where value is located, or -1 on failure
+ */
+
+int advanced_binary(int *array, size_t size, int value)
+{
+	size_t low = 0, high = size - 1, mid, i;
+
+	if (array == NULL || size == 0)
+		return (-1);
+
+	while (low <= high)
+	{
+		printf("Searching in array: ");
+		for (i = low; i <= high; i++)
+		{
+			printf("%d", array[i]);
+			if (i < high)
+				printf(", ");
+			else
+				printf("\n");
+		}
+		mid = (low + high) / 2;
+		if (array[mid] == value && array[mid - 1] != value)
+			return (mid);
+		else if (array[mid] < value)
+			low = mid + 1;
+		else
+			high = mid;
+	}
+	return (-1);
+}
